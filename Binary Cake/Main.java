32c8e246068b8d7bc016e3291f943049861aa22b
Mehyar44 <mehyarsalem44@gmail.com>
2024-07-03 15:21:34 -0400

    Create Main.java

---
+++ Binary Cake/Main.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner s = new Scanner (System.in);
+    System.out.print("Enter your age: ");
+    int age = s.nextInt();
+    System.out.println();
+    binaryCake(age);
+    s.close();
+  }
+  
+  public static void binaryCake(int age) {
+    int[] cake = {0,0,0,0,0,0,0};
+    for (int i=0; i<7; i++) {
+      if (age >= Math.pow(2,6-i)) {
+        cake[i]=1;
+        age -= Math.pow(2,6-i);
+      }
+    }
+      
+    for (int i:cake) {
+      if (i==1) System.out.print("🔥 ");
+      else System.out.print("⬛️ ");
+    }
+    System.out.println();
+
+    String[] candles = {"🟥 ","🟧 ","🟨 ","🟩 ","🟦 ","🟪 ", "⬜️"};
+    for (int j=0; j<2; j++) {
+      for (String square : candles) {System.out.print(square);}
+      System.out.println();
+    }
+
+    for (int j=0; j<5; j++) {
+      for (int i=0; i<10; i++) {System.out.print("🟫");}
+      System.out.println();
+    }
+
+    System.out.print("\nHAPPY BIRTHDAY!!!!");
+  }
+}
