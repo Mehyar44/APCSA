a2df13757d7611c47559de82d2232f2e8a267710
Mehyar44 <mehyarsalem44@gmail.com>
2024-07-04 21:04:35 -0400

    Create Cards.java

---
+++ Cards/Cards.java
@@ -0,0 +1,19 @@
+public class Card {
+  private String character;
+  private String symbol;
+
+  public Card(String c, String s, int v) {
+    character = c;
+    symbol = s;
+  }
+
+  public String toString() {
+    String result = "";
+    if (symbol.equals("spades")) result += "\u001B[47m\u001B[30m" + character + "♠\u001B[0m";
+    else if (symbol.equals("clubs")) result += "\u001B[47m\u001B[30m" + character + "♣\u001B[0m";
+    else if (symbol.equals("diamonds")) result += "\u001B[47m\u001B[31m" + character + "♦\u001B[0m";
+    else result += "\u001B[47m\u001B[31m" + character + "♥\u001B[0m";
+    
+    return result;
+  }
+}
