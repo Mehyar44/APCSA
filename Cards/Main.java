84e8d07e3429dfaba8cdae09a6f6e96f83071dae
Mehyar44 <mehyarsalem44@gmail.com>
2024-07-09 13:13:43 -0400

    Update Main.java

--- Cards/Main.java
+++ Cards/Main.java
@@ -2,12 +2,11 @@ public class Main {
   public static void main(String[] args) {
     String[] characters = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
     String[] symbols = {"spades", "clubs", "diamonds", "hearts"};
-    int[] values = {11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10}; 
 
     for (String symbol : symbols) {
       String line = null;
       for (int i = 0; i < characters.length; i++) {
-        Card card = new Card(characters[i], symbol, values[i]);
+        Card card = new Card(characters[i], symbol);
         if (line != null) line += " ";
         else line = "";
         line += card.toString();
